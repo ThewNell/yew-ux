301cb35acfff23aee221ac3d003f1f0f6b222450
Thew <98503270+ThewNell@users.noreply.github.com>
2022-11-25 16:22:11 -0700

    * added workspace projects to repo

---
+++ component-lib/src/main.rs
@@ -0,0 +1,13 @@
+use yew::prelude::*;
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
+
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <h1>{ "Hello World!" }</h1>
+    }
+}
